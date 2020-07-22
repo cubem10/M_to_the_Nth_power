2a3470d3a01e4298ffa293af766ffc5acd805492
cubem10 <60423448+cubem10@users.noreply.github.com>
2020-07-22 21:38:57 +0900

    Fixed wrong elapsed time

--- M_to_the_Nth_power.py
+++ M_to_the_Nth_power.py
@@ -1,7 +1,7 @@
 import time
-start_time = time.time()
 ina = int(input('Enter the value of m when m^n.: '))
 inb = int(input('Enter the value of n when m^n.: '))
+start_time = time.time()
 out = 1
 for i in range(0, inb):
 	out = out * ina
