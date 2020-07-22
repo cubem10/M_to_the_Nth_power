84e9c34faa5f6b613b5c02f76fa3436fa7fc56af
cubem10 <60423448+cubem10@users.noreply.github.com>
2020-07-22 21:32:15 +0900

    Add elapsed time information

--- M_to_the_Nth_power.py
+++ M_to_the_Nth_power.py
@@ -1,3 +1,5 @@
+import time
+start_time = time.time()
 ina = int(input('m^n을 할 때, m의 값: '))
 inb = int(input('m^n을 할 때, n의 값: '))
 out = 1
@@ -5,3 +7,4 @@ for i in range(0, inb):
 	out = out * ina
 print(out)
 print(len(str(out)), "digits")
+print("Total elapsed time: ", (time.time() - start_time))
